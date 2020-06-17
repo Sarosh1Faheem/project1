8a2fe026c0256ae0b7820ec4c97621c1ea379cac
Sarosh Faheem <52979134+Sarosh1Faheem@users.noreply.github.com>
2020-06-17 17:18:51 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import My_intro from './sarosh.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      <My_intro name=" Faheem" course="PIAIC" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/sarosh.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function My_intro(props){
+    return(
+    <h1>Hello, World.!, From Sarosh{props.name}, Studying {props.course}</h1>
+    );
+}
+
+export default My_intro;
